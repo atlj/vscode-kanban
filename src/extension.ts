9cb44b7327a1b092dd6fcbdb898ca1daad651290
Burak <md.burak.guner@gmail.com>
2022-07-30 03:31:58 +0300

    feat: list boards in the current workspace

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,55 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { TreeDataProvider } from "./treeDataProvider";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-kanban" is now active!');
+  console.log("im alive");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-kanban.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-kanban!');
-	});
+  if (vscode.workspace.workspaceFolders === undefined) {
+    return;
+  }
 
-	context.subscriptions.push(disposable);
+  const boardsPath = vscode.Uri.joinPath(
+    vscode.workspace.workspaceFolders[0].uri,
+    ".vscode",
+    "kanban"
+  );
+
+  const treeDataProvider = new TreeDataProvider(boardsPath);
+
+  vscode.window.registerTreeDataProvider(
+    "vscode-kanban.activityView",
+    treeDataProvider
+  );
+
+  const disposableRefreshBoards = vscode.commands.registerCommand(
+    "vscode-kanban.refreshBoards",
+    () => {
+      treeDataProvider.refresh();
+    }
+  );
+
+  function checkBoards() {
+    treeDataProvider.refresh();
+  }
+
+  const disposableCreate = vscode.workspace
+    .createFileSystemWatcher(vscode.Uri.joinPath(boardsPath, "*.json").path)
+    .onDidCreate(checkBoards);
+
+  const disposableDelete = vscode.workspace
+    .createFileSystemWatcher(vscode.Uri.joinPath(boardsPath, "*.json").path)
+    .onDidDelete(checkBoards);
+
+  const disposableChange = vscode.workspace
+    .createFileSystemWatcher(vscode.Uri.joinPath(boardsPath, "*.json").path)
+    .onDidChange(checkBoards);
+
+  context.subscriptions.push(
+    disposableCreate,
+    disposableDelete,
+    disposableChange,
+    disposableRefreshBoards
+  );
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
---
+++ src/treeDataProvider.ts
@@ -0,0 +1,68 @@
+import * as vscode from "vscode";
+
+export class TreeDataProvider implements vscode.TreeDataProvider<BoardItem> {
+  constructor(private boardsUri: vscode.Uri) {}
+
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    BoardItem | undefined | null | void
+  > = new vscode.EventEmitter<BoardItem | undefined | null | void>();
+
+  onDidChangeTreeData?:
+    | vscode.Event<void | BoardItem | BoardItem[] | null | undefined>
+    | undefined = this._onDidChangeTreeData.event;
+
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element: BoardItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    return element;
+  }
+
+  getChildren(
+    element?: BoardItem | undefined
+  ): vscode.ProviderResult<BoardItem[]> {
+    return vscode.workspace.fs
+      .readDirectory(this.boardsUri)
+      .then((files) =>
+        files.filter(
+          (file) =>
+            file[1] === vscode.FileType.File && file[0].endsWith(".json")
+        )
+      )
+      .then((files) => {
+        if (files.length === 0) {
+          vscode.commands.executeCommand(
+            "setContext",
+            "vscode-kanban.noBoards",
+            true
+          );
+        } else {
+          vscode.commands.executeCommand(
+            "setContext",
+            "vscode-kanban.noBoards",
+            false
+          );
+        }
+
+        return files;
+      })
+      .then((files) =>
+        files.map(
+          (file) =>
+            new BoardItem({
+              uri: vscode.Uri.joinPath(this.boardsUri, file[0]),
+              label: file[0].replace(".json", ""),
+            })
+        )
+      );
+  }
+}
+
+class BoardItem extends vscode.TreeItem {
+  constructor({ uri, label }: { uri: vscode.Uri; label: string }) {
+    super(label);
+    this.resourceUri = uri;
+    this.tooltip = uri.path;
+  }
+}
